393beea1d50b8e762af643deed94e0d0afc48817
Sergei Iurchenko <banzach18@gmail.com>
2025-09-07 17:05:05 +0300

    Fixed according to comments

--- Homework_1.py
+++ Homework_1.py
@@ -1,27 +1,29 @@
 # 1) Найти минимальное число в списке (можно создать любой список чисел)
 
-listOfNumbers = [3,2,3,12,5,6,10, 1.2,]
+numbers = []
+# обрабатываем случай, если лист пуст
+if numbers:
 
 # через функцию min
-print('Минимальное число в списке через min: ', min(listOfNumbers))
+    print('Минимальное число в списке через min: ', min(numbers))
 
 # через перебор значений
-minNumber = listOfNumbers[0]
-for i in listOfNumbers:
-    if i < minNumber:
-        minNumber = i
-print('Минимальное число в списке через перебор: ', minNumber)
+    minNumber = numbers[0]
+    for i in numbers:
+        if i < minNumber:
+            minNumber = i
+    print('Минимальное число в списке через перебор: ', minNumber)
 
 # Не понял как сделать тоже самое через list comprehension. Ругается на minNumber=i, но не понимаю как это написать по-другому
-# print([minNumber=i for i in listOfNumbers if i < minNumber])
+# print([minNumber=i for i in numbers if i < minNumber])
 
 
 # 2) Посчитать количество цифр в строке (например в такой "Сегодня 05.09.2025") - в питоне есть метод для проверки на число isdigit() он тебе поможет)
 text = "Сегодня 05.09.2025"
 counter = 0
-for i in text:
-    if i.isdigit():
-          counter += 1
+for char in text:
+    if char.isdigit():
+        counter += 1
 print('Количество цифр в строке:', text, '= ', counter)
 
 
@@ -29,7 +31,7 @@ print('Количество цифр в строке:', text, '= ', counter)
 x = int(input('Введите число: '))
 f = 1
 i = 1
-while i < x:
-    f = f * (i+1)
+while i <= x:
+    f *= i
     i += 1
 print('! ',x, '= ', f)
