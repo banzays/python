fe95ba29cf591bfff6236dabec74dd3ee0d695cc
Sergei Iurchenko <banzach18@gmail.com>
2025-10-09 14:56:07 +0300

    Corrections in Homework_4

--- Homework_4.py
+++ Homework_4.py
@@ -15,30 +15,31 @@ def time_decorator(func):
 @time_decorator
 def calculate(operation, *numbers):
     if operation == 'sum':
-        return print(f'Сумма всех чисел: {sum(numbers)}')
+        print(f'Сумма всех чисел: {sum(numbers)}')
+        return sum(numbers)
     elif operation == 'mul':
-        return print(f'Произведение всех чисел: {math.prod(numbers)}')
+        print(f'Произведение всех чисел: {math.prod(numbers)}')
+        return math.prod(numbers)
     else:
-        return print('Выберите операцию "sum" или "mul"')
+        print('Выберите операцию "sum" или "mul"')
+        return None
+
 calculate('sum', 1,2,3,4)
 
 
 # 2) Напиши функцию update_dict, которая принимает словарь и любое количество именованных аргументов. Функция должна обновлять словарь значениями из kwargs и возвращать новый словарь.
 @time_decorator
 def update_dict(dicts, **kwargs):
-    dicts.update(kwargs)
-    return dicts
+    upd_dicts = dicts.copy()
+    upd_dicts.update(kwargs)
+    return upd_dicts
 
 update_dict({'name':'Siroja', 'age':'31'}, nationality='Russian')
 
 # 3) Напиши функцию filter_list, которая принимает список data и набор значений через *args. Функция должна возвращать новый список, в котором исключены все элементы из data, которые встречаются в args.
 @time_decorator
 def filter_list(data, *args):
-    data = set(data)
-    for i in args:
-        if i in data:
-            data.remove(i)
-    return list(data)
+    return list(set(data) - set(args))
 
 print(filter_list([1,2,3,4,1,'apple'],'banana', 'apple', 1, 9, 8))
 
